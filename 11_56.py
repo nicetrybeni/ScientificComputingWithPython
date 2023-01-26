6eac5e9c79673032a87599ba4f41d6c4fb776ed2
Joseph Benipayo <101826290+benipayoj@users.noreply.github.com>
2023-01-27 03:47:30 +0800

    Update 11_56.py

--- 11_56.py
+++ 11_56.py
@@ -1,10 +1,11 @@
 # I'm such a sht for being lazy
-# stop being lazy bro.
-# still lazy and unworthy.
-# still not improving & still unworthy
-# watch me watch me nae nae
-# still not improving 
-# I think I should quit
-# nvm ill try again, hehe
-# I need to send output to DB
-# too lazy, sure ball
+xh = input("Enter the value of X: ")
+ih = int(xh)
+x = ih
+if x < 2 :
+    print("Small")
+elif x < 10 :
+    print("Medium")
+else : 
+    print("Large")
+print("All Done")
