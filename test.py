9368912990a19bb83b586ad81133f1a0c24f7cab
Joseph Benipayo <101826290+benipayoj@users.noreply.github.com>
2023-05-27 01:44:24 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,9 @@
+name = input("What is your name: ")
+
+match name:
+    case "beni":
+            print("MASTER BENI")
+    case "duqs":
+            print("Pakyu duque")
+    case _:
+            print("PAKYUALL")
