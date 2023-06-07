cd93e3fc8334633af7797b35ecb2627be31c5ae1
Joseph Benipayo <101826290+benipayoj@users.noreply.github.com>
2023-06-07 21:45:17 +0800

    Update testknowledge.py

--- testknowledge.py
+++ testknowledge.py
@@ -1,5 +1,9 @@
-
-value = [10, 6, 30 , 20] 
-ans = sum(value)
-
-print(ans)
+x = 0
+if x > 10 :
+    print("LOWER")
+elif x > 5 :
+    print("Too low")
+elif x == 5:
+    print("Match")
+else :
+    print("WEIRD")
