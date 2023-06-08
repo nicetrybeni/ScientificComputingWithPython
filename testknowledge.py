b763d7f950ff4eae9d077270b8d2634615cd7d3e
Joseph Benipayo <101826290+benipayoj@users.noreply.github.com>
2023-06-08 22:38:45 +0800

    Update testknowledge.py

--- testknowledge.py
+++ testknowledge.py
@@ -1,9 +1,5 @@
-x = 0
-if x > 10 :
-    print("LOWER")
-elif x > 5 :
-    print("Too low")
-elif x == 5:
-    print("Match")
+x = 5 
+if x > 6 : 
+    print("Greater than")
 else :
-    print("WEIRD")
+    print("I dont know")
