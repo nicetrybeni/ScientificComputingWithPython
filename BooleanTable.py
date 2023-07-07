430ce3c07548f418150dba97c962895466ab6a21
Joseph Benipayo <101826290+benipayoj@users.noreply.github.com>
2023-07-07 22:46:42 +0800

    Boolean Table

---
+++ BooleanTable.py
@@ -0,0 +1,18 @@
+#Boolean Table using AND and OR
+#True and True = True
+#True and False = False
+#False and True = False
+#False and False = False
+
+#True or True = True
+#True or False = True
+#False or True = True
+#Falase or False = False
+
+#Boolean Operator using NOT
+# not True = False
+# not False = True
+A = True
+B = False 
+C = A or B
+print(C)
--- test.py
+++ test.py
@@ -1,12 +1,4 @@
-passwordFile = open('secretPasswordFile.txt', 'r')
-secretPassword = passwordFile.read()
-passwordFile.close()
-
-print('Enter your assword.')
-typedPassword = input().strip()
-if typedPassword == secretPassword:
-    print('Access Granted')
-elif typedPassword == '12345':
-    print('That password is for BOBO')
-else:
-    print('Access Denied')
+total = 0
+for num in range(101):
+    total = total + num
+print(total)
